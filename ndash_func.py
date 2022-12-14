c42a2472a15423cf5ad1f4a401768a9fc8144717
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 14:48:22 +0200

    init

---
+++ ndash_func.py
@@ -0,0 +1,156 @@
+# Functions to retrieve, extract, and transform US Census Bureau
+# Zip Code Tabulation Data
+
+# import packages
+import urllib2
+import requests
+import urllister
+import zipfile
+import sys
+import os, os.path
+
+###############
+# Checks to see if there is a zip for the current year
+# If it does not exist, then decrements year until a
+# file is found
+def findFile(year, dataset):
+    data_path="https://www2.census.gov/geo/tiger/TIGER"
+
+    success=False
+    print(data_path + str(year) + "/" + dataset)
+    
+    while success==False:
+        try:
+          print(data_path + str(year) + "/" + dataset)
+          con=urllib2.urlopen(data_path + str(year) + "/" + dataset)
+        except urllib2.HTTPError, e:
+          print(e)
+          print("Decrementing year and trying again.")
+          year = year -1
+        except urllib2.URLError, e:
+          print(e)
+          print("Decrementing year and trying again.")
+          year = year -1
+        else:
+            print("Connection a success!")
+            success=True
+            parser=urllister.URLLister()
+            parser.feed(con.read())
+            con.close()
+            parser.close()
+            zipfile=[s for s in parser.urls if ".zip" in s]
+
+    # if there are more than 1 zip files, look for NYS
+    if len(zipfile) > 1:
+      ny_i = [i for i, s in enumerate(zipfile) if '_36_' in s][0]
+      zipfile = zipfile[ny_i]
+
+    file_url=data_path + str(year) + "/"+ dataset + "/" + ''.join(zipfile)
+
+    return file_url
+
+###############
+# Checks to see if the file exists
+def fileExists(file_url):
+  filename=file_url.split('/')[-1]
+  if os.path.isfile(filename):
+    return True
+  else:
+    return False
+
+###############
+# Takes a url and downloads the file to the local repository
+def downloadFile(file_url):
+    counter=0
+
+    while not fileExists(file_url):
+        if file_url.endswith('.zip'):
+          filename=downloadZip(file_url)
+        else:
+          filename=downloadUrl(file_url)
+
+        if counter > 3:
+          print('There is a problem downloading. Exiting.')
+          break
+        counter+=1
+
+###############
+# Used when the URL is a direct source of the file
+def downloadZip(file_url):
+	con=urllib2.urlopen(file_url)
+
+	filename=file_url.split('/')[-1]
+	save_file=open(filename, "wb")
+  	save_file.write(con.read())
+
+	# print('Downloading ' + filename)
+ #        total_size = con.info().getheader('Content-Length').strip()
+	# total_size = int(total_size)
+
+	# bytes_so_far = 0
+
+	# save_file=open(filename, "wb")
+
+	# while bytes_so_far < total_size:
+	#     buff = con.read(8192)
+	#     save_file.write(buff)
+	#     bytes_so_far += len(buff)
+
+	#     percent = float(bytes_so_far) / total_size
+	#     percent = round(percent*100, 2)
+	#     sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % (bytes_so_far, total_size, percent))
+
+	# save_file.close()
+
+	# if bytes_so_far == total_size:
+	# 	print(filename + ' successfully downloaded!')
+	# 	return filename
+	return filename
+###############
+# Used when the URL triggers a download of a zip file
+def downloadUrl(file_url):
+	req=requests.get(file_url)
+
+	cd = req.headers['Content-Disposition'].split('"')
+	index_match=[i for i, s in enumerate(cd) if ".zip" in s]
+	filename=cd[index_match[0]]
+
+	if os.path.isfile(filename):
+		print(filename + ' already exists!')
+		return filename
+	else:
+		print('Downloading ' + filename)
+		
+		with open(filename, 'wb') as f:
+			f.write(req.content)
+
+		print(filename + ' successfully downloaded!')
+
+		return filename
+
+###############
+# Extract the zip file
+def extractFile(file_url):
+  current_dir= os.getcwd()
+
+  filename=file_url.split('/')[-1];
+
+  # output_dir=os.path.join(current_dir, os.path.splitext(filename)[0])
+  output_dir=os.path.join(current_dir, 'shapefiles')
+	
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+	# check to see that the file exists, or exit
+  if not os.path.isfile(filename):
+    print('Cannot find + ' + filename)
+    sys.exit()
+
+  print('Beginning extraction')
+
+  # zipfile.ZipFile(filename).extractall(output_dir)
+  zipfile.ZipFile(filename).extractall()
+  print(filename + ' extraction complete!')
+
+  return filename.split('.zip')[0]
+
---
+++ poverty.py
@@ -0,0 +1,103 @@
+# Gets the NYCgov poverty rate at the community district level
+import urllib2
+import urllister
+import re
+import pandas as pd
+import ast
+
+###
+# Gets the NYCgov Poverty Rate data from NYCO website
+###
+def getNYCGovPoverty():
+  # read the data from the nycopp site
+  jsfile = "https://www1.nyc.gov/assets/opportunity/js/agencies/cd_data.js"
+  con = urllib2.urlopen(jsfile)
+  nycgov_str = con.read()
+  con.close()
+
+  # replace strings
+  nycgov_str = re.sub("cd_data\[[0-9]*\]\ =\ ", "", nycgov_str)
+  nycgov_str = re.sub(";", "", nycgov_str)
+
+  # convert strings to lists
+  nycgov_list = nycgov_str.split('\n')
+  nycgov_list.pop(0)
+  nycgov_list.pop(len(nycgov_list)-1)
+
+  nycgov_list = [ast.literal_eval(i) for i in nycgov_list]
+
+  # create a dataframe
+  nycgov_df = pd.DataFrame(nycgov_list)[[3, 11, 12]]
+
+  # only get the data that is 100% under poverty
+  nycgov_df = nycgov_df.loc[nycgov_df[3] == 'POVERTY']
+  
+  # if the community district column does not contain any of the boroughs, then, remove it from the dataframe
+  nycgov_df = nycgov_df[~nycgov_df[11].str.contains(
+      'Manhattan|Bronx|Brooklyn|Queens|Staten') == False]
+
+  nycgov_df[[11, 'Neighborhood']] = nycgov_df[11].str.split(' \(', expand=True)
+  nycgov_df[11] = nycgov_df[11].str.replace('Staten Island', 'StatenIsland')
+
+  temp_df = nycgov_df[11].str.split(" ", 1, expand=True)
+  temp_df[1] = temp_df[1].str.replace(' & ', ',')
+
+  # prefix community district code with Manhattan
+  temp_df[1] = temp_df[1].str.replace(r'(?is)[0-9]+2', r'M\g<0>')
+  temp_df[1] = temp_df[1].str.replace(r'(?is)\b\d\b', r'M0\g<0>')
+  temp_df[1] = temp_df[1].str.replace(r'^[0-9]{1,2}', r'M\g<0>')
+
+  # update the community district codes
+  temp_df.loc[temp_df[0] == 'Bronx', 1] = temp_df[1].str.replace('M', 'B')
+  temp_df.loc[temp_df[0] == 'Brooklyn', 1] = temp_df[1].str.replace('M', 'K')
+  temp_df.loc[temp_df[0] == 'Queens', 1] = temp_df[1].str.replace('M', 'Q')
+  temp_df.loc[temp_df[0] == 'StatenIsland', 1] = temp_df[1].str.replace('M', 'S')
+
+  nycgov_df = nycgov_df.join(temp_df)
+  collist = [12, 1]
+  nycgov_df = nycgov_df[collist]
+
+  nycgov_df.rename(columns={
+      12: 'nycgov_cd_pov_rate',
+      1: 'community_districts'
+  }, inplace=True)
+
+  return nycgov_df
+
+###
+# Connects to the Census API and gets the population in poverty and total pop, calculates the poverty rate
+###
+def getCensusPoverty(year, zipcodes):
+  success = False
+  while success == False:
+    try:
+      url = 'https://api.census.gov/data/' + str(year) + '/acs/acs5?get=B06012_002E,B06012_001E&for=zip+code+tabulation+area:' + str(zipcodes)
+      print('Connecting to ' + url)
+      con = urllib2.urlopen(url)
+    except urllib2.HTTPError, e:
+      print(e)
+      year = year - 1
+    else:
+      print("Connection a success!")
+      print(year)
+      success = True
+      census_data = con.read()
+      con.close()
+
+  # make a call to the api to see if anything returns. If it doesn't decrement the year
+  census_data = ast.literal_eval(census_data)
+  census_data.pop(0)
+
+  # convert the response into a dataframe
+  census_df = pd.DataFrame(census_data)
+  census_df.rename(columns={
+      0: 'census_total_pov',
+      1: 'census_total_pop',
+      2: 'zcta'
+  }, inplace=True)
+
+  # create the poverty rate column by dividing total in pov by the total pop
+  census_df['census_pov_rate'] = (pd.to_numeric(
+      census_df['census_total_pov'])/pd.to_numeric(census_df['census_total_pop']))*100
+
+  return census_df
